d1bb0af9fc965fd13924797097fb45b89125663e
Mikkel Trolle Larsen <trolster2@gmail.com>
2016-06-13 13:30:51 +0200

    Add command for setting the token: <token> <theToken>

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 ### Description
 A CLI for configuring and running API calls.
 
+### Setup
+1. Start by running `rqcli token <yourToken>`. This will create the `apiConfig.json` file in which your account information will be stored.
+
 ### Project Styleguide
 
 [![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log('Hello, world!');
+const fs = require('fs')
+const program = require('commander')
+const config = require('./apiConfig')
+
+program
+  .command('token <token>')
+  .description('set the token')
+  .action(token => {
+    config.token = token
+    fs.writeFileSync('apiConfig.json', JSON.stringify(config, null, 2))
+  })
+
+program.parse(process.argv)
