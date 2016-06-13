3fd8a3e0717a0f82889fb53df9815c59d4c2ced5
Mikkel Trolle Larsen <trolster2@gmail.com>
2016-06-13 16:47:22 +0200

    Add <assigned> command whith desktop notifications

--- README.md
+++ README.md
@@ -4,19 +4,29 @@
 ### Description
 A CLI for configuring and running API calls.
 
+### Installation
+**IMPORTANT** This project has not been published to npm yet, so the code below won't work.
+```shell
+npm install rqcli -g
+```
+
 ### Setup
 1. Start by running `rqcli token <yourToken>`. This will create the `apiConfig.json` file in which your account information will be stored.
 2. Run `rqcli certs` which will get the projects you are certified for and save them in the config.
+3. Check if you have any submissions currently assigned by running `rqcli assigned`.
 
 ### CLI commands
 
 **token**
-_Stores an API Auth token_
-- Argument: `<token>`, your token which you can copy from your dashboard > API Access.
+- _Stores an API Auth token_
+- Arguments: `<token>`, your token which you can copy from your dashboard > API Access.
 
 **certs**
-_Displays all of the project names with ids for which you are certified._
+- _Displays all of the project names with ids for which you are certified._
 - Options: `-u`, `--update`, updates your certifications.
+
+**assigned**
+-_Notifies the user of submissions that are assigned to them._
 
 ### Project Styleguide
 
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const path = require('path')
 const program = require('commander')
+const notifier = require('node-notifier')
 const apiCall = require('./apiCall')
 const config = require('./apiConfig')
 
+/**
+* @desc Accepts a token and saves it to the config file.
+*/
 program
   .command('token <token>')
   .description('set the token')
@@ -13,6 +18,10 @@ program
     fs.writeFileSync('apiConfig.json', JSON.stringify(config, null, 2))
   })
 
+/**
+* @desc Logs the users certifications to the console.
+* Options: --update, updates the certifications and logs them to the console.
+*/
 program
   .command('certs')
   .option('-u, --update', 'update certificatons')
@@ -38,10 +47,37 @@ program
     }
   })
 
-program.parse(process.argv)
-
 function showCerts () {
   config.certified.forEach(elem => {
     console.log(`Project Name: ${elem.name}, Project ID: ${elem.id}`)
   })
 }
+
+/**
+* @desc Sends a desctop notifications to the user with the name of the projects
+* that have been assigned and the id. It opens the review page for the
+* submission if you click on the notification.
+*/
+program
+  .command('assigned')
+  .description('get the submissions that are assigned to you')
+  .action(() => {
+    apiCall('assigned')
+      .then(res => {
+        if (res.body.length) {
+          res.body.forEach(sub => {
+            notifier.notify({
+              title: 'Currently Assigned:',
+              message: `${sub.project.name}, ID: ${sub.id}`,
+              open: `https://review.udacity.com/#!/submissions/${sub.id}`,
+              icon: path.join(__dirname, 'clipboard.svg'),
+              sound: 'Ping'
+            })
+          })
+        } else {
+          console.log('No reviews are assigned at this time.')
+        }
+      })
+  })
+
+program.parse(process.argv)
