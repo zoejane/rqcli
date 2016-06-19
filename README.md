51b4bdc8d5778c9d500c53c1174314604257cc2e
Mikkel Trolle Larsen <trolster2@gmail.com>
2016-06-19 13:30:18 +0200

    Add assign <projectId> [moreIds...] command

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ npm install rqcli -g
 ### CLI commands
 
 **token**
-- _Stores an API Auth token_
+- _Stores an API Auth token and the day-of-year to be able to calculate the tokens age_
 - Arguments: `<token>`, your token which you can copy from your dashboard > API Access.
 
 **certs**
@@ -26,7 +26,12 @@ npm install rqcli -g
 - Options: `-u`, `--update`, updates your certifications.
 
 **assigned**
--_Notifies the user of submissions that are assigned to them._
+- _Notifies the user of submissions that are assigned to them._
+
+**assign**
+- _Starts requesting the Udacity Review API queue for assignments of the type specified in the commands arguments._
+- Arguments: `<projectId> [moreIds...]`, space separated list of project ids to request for.
+- Tip: You can use the list of arguments to weigh the requested projects. If for instance, your list looked like this `rqcli assign 144 144 134 4`, the project `144` would take up half of all the calls to the API.
 
 ### Project Styleguide
 
--- index.js
+++ index.js
@@ -2,10 +2,106 @@
 
 const fs = require('fs')
 const path = require('path')
+const moment = require('moment')
 const program = require('commander')
 const notifier = require('node-notifier')
 const apiCall = require('./apiCall')
 const config = require('./apiConfig')
+
+
+/**
+* @desc Starts requesting the Udacity Review API queue for assignments. Accepts
+* a space separated list of project ids to request for.
+*/
+program
+  .command('assign <projectId> [moreIds...]')
+  .description('poll the API queue for submissions')
+  .action((projectId, moreIds) => {
+    // How often we check the number of submissions that are already assigned.
+    const reqAssignedInterval = 30
+    let ids = [projectId]
+    if (moreIds) {
+      moreIds.forEach(id => ids.push(id))
+    }
+    let renewToken = false
+    let secondsSinceStart = 0
+    let callsTotal = 0
+    let assigned = 0
+    let reqAssignedIn = 0
+    // Since the user can add the same id multiple times to weigh the requests
+    // to a particular project, we have to validate the Set() of the ids.
+    validateProjectIds(new Set(ids))
+    requestNewAssignment()
+    /**
+    * @desc Every 30 seconds it checks how many submissions are currently assigned
+    * to the user. If it's 2, it waits the length of the interval const and checks
+    * again. As long as it's less than 2 it requests a new assignment every second.
+    */
+    function requestNewAssignment () {
+      // Test the age of the token every one days
+      if (!renewToken && !(secondsSinceStart % 86400)) {
+        console.log('Checking renewal')
+        if (moment().dayOfYear - config.tokenAge > 25) {
+          renewToken = true
+        }
+      }
+      if (reqAssignedIn === 0) {
+        console.log('checking assigned')
+        apiCall('assigned')
+          .then(res => {
+            if (res.body.length) {
+              assigned = res.body.length
+            } else {
+              console.log('No reviews are assigned at this time.')
+            }
+          })
+      } else {
+        if (assigned === 2) {
+          console.log(`Max submissions assigned. Checking again in ${reqAssignedInterval - reqAssignedIn} seconds.`)
+        } else {
+          let id = ids[callsTotal % ids.length]
+          console.log(`Requesting assignment for project ${id}`)
+          callsTotal++
+          apiCall('assign', 'POST', id)
+            .then(res => {
+              if (res.statusCode === 201) {
+                let projectName = res.body.project.name
+                let submissionId = res.body.id
+                notifier.notify({
+                  title: 'New Review Assigned!',
+                  message: `${projectName}, ID: ${submissionId}`,
+                  open: `https://review.udacity.com/#!/submissions/${submissionId}`,
+                  icon: path.join(__dirname, 'clipboard.svg'),
+                  sound: 'Ping'
+                })
+              } else if (res.statusCode !== 404) {
+                console.log(`Server responded with an error: ${res.statusCode}`)
+              }
+              callsTotal++
+            })
+        }
+      }
+      setTimeout(() => {
+        console.log('setting timeout')
+        secondsSinceStart++
+        reqAssignedIn = moment().seconds() % reqAssignedInterval
+        requestNewAssignment()
+      }, 1000)
+    }
+  })
+
+/**
+* Throws an error if not all of the ids are in the list of certified projects.
+*/
+function validateProjectIds (ids) {
+  let certified = new Set(config.certified.map(project => project.id))
+  let intersection = new Set([...ids].filter(id => certified.has(id)))
+  if (intersection.size < ids.size) {
+    let difference = new Set([...ids].filter(id => !intersection.has(id)))
+    console.log(`Not certified for the following projects ${[...difference].join(', ')}`)
+    throw new Error(`Illegal Action: Not certified.`)
+  }
+}
 
 /**
 * @desc Accepts a token and saves it to the config file.
@@ -15,6 +111,7 @@ program
   .description('set the token')
   .action(token => {
     config.token = token
+    config.tokenAge = moment().dayOfYear()
     fs.writeFileSync('apiConfig.json', JSON.stringify(config, null, 2))
   })
 
@@ -35,7 +132,7 @@ program
             if (elem.status === 'certified') {
               config.certified.push({
                 name: elem.project.name,
-                id: elem.project_id
+                id: elem.project_id.toString()
               })
             }
           })
