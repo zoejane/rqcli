02af874d0ce76f25c692349361d38c8b6c488598
Mikkel Trolle Larsen <trolster2@gmail.com>
2016-08-05 01:12:46 +0200

    Return the full body of the request

--- commands/assigned.js
+++ commands/assigned.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
 const apiCall = require('../utils').apiCall
-const path = require('path')
+const moment = require('moment')
 const notifier = require('node-notifier')
 
-function desktopNotification (assigned) {
-  assigned.forEach(sub => {
+function desktopNotification (reviews) {
+  reviews.forEach(review => {
+    let when = moment(review.assigned_at).format('HH:mm')
+
     notifier.notify({
-      title: 'New Review Assigned!',
-      message: `${sub.projectName}, ID: ${sub.id}`,
-      open: `https://review.udacity.com/#!/submissions/${sub.id}`,
-      icon: path.join(__dirname, 'clipboard.svg'),
+      title: `Project: ${review.project.name}: `,
+      message: `Assigned: ${when}, ID: ${review.id}`,
+      open: `https://review.udacity.com/#!/submissions/${review.id}`,
       sound: 'Ping'
     })
   })
@@ -19,22 +20,16 @@ function desktopNotification (assigned) {
 /**
 * Gets information on currently assigned submissions, if the user has any.
 * @param: token The users active token.
-* @returns: {Array} assigned A list of assigned submissions.
+* @returns: {Array} res.body A list of submissions assigned for review.
 */
 module.exports = ({auth: {token}}, options) => {
   return new Promise((resolve, reject) => {
     apiCall(token, 'assigned')
     .then(res => {
-      let assigned = res.body.map(sub => {
-        return {
-          projectName: sub.project.name,
-          id: sub.id
-        }
-      })
       if (options.notify) {
-        desktopNotification(assigned)
+        desktopNotification(res.body)
       }
-      resolve(assigned)
+      resolve(res.body)
     })
   })
 }
