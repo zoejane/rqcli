e9a9e5a137f0ee80aaeb2310b4bd0513b13c268b
Mikkel Trolle Larsen <trolster2@gmail.com>
2016-08-18 00:12:28 +0200

    Add feedback on error

--- commands/certs.js
+++ commands/certs.js
@@ -6,19 +6,33 @@ module.exports = ({certs, auth: {token}}, options) => {
   const update = options.update || !certs.certified
   return new Promise((resolve, reject) => {
     if (update) {
+      console.log('Updating certifications...')
       apiCall(token, 'certifications')
         .then(res => {
-          let updatedCerts = res.body
-            .filter(cert => cert.status === 'certified')
-            .map(cert => {
-              return {
-                name: cert.project.name,
-                id: cert.project_id.toString()
-              }
-            })
-          certs.save(updatedCerts)
-          certs.show(updatedCerts)
-          resolve()
+          if (res.statusCode === 200) {
+            let updatedCerts = res.body
+              .filter(cert => cert.status === 'certified')
+              .map(cert => {
+                return {
+                  name: cert.project.name,
+                  id: cert.project_id.toString()
+                }
+              })
+            certs.save(updatedCerts)
+            certs.show(updatedCerts)
+            resolve()
+          } else if (res.statusCode === 500) {
+            console.log('An error has occured.')
+            console.log('This is probably because of a bad/outdated token.')
+            reject(`    Internal Server Error: statusCode: ${res.statusCode}.`)
+          } else {
+            reject(`Error: statusCode: ${res.statusCode}`)
+          }
+        })
+        .catch(err => {
+          // If the API call returns an error.
+          console.log(`API call returned an error: ${err}`)
+          process.exit(0)
         })
     } else {
       certs.show(certs.certified)
